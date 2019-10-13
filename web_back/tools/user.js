f91e099f0a9bee3bbcfa9c9777e6b5a8ecfd950e
gaojie <295626357@qq.com>
2019-10-13 20:04:01 +0800

    进行了user.js的创建和登录界面的优化

---
+++ web_back/tools/user.js
@@ -0,0 +1,19 @@
+// 在里面进行请求信息的设置
+
+var user = {
+    // 登录请求
+    login: function (options) {
+        $.ajax({
+            type: 'post',
+            // 因为如果前后端要进行修改地址的话，单个操作麻烦，进行js封装
+            url: USER_LOGIN,
+            data: options.data,
+            success: options.callback
+
+        });
+    },
+
+    // 退出请求
+    
+
+};
