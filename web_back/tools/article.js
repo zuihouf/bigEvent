be2a3b5cd8f43cad54269ab74fa2cd9fd86e7f68
gaojie <295626357@qq.com>
2019-10-16 20:40:24 +0800

    分类数据的优化

--- web_back/tools/article.js
+++ web_back/tools/article.js
@@ -1,3 +1,53 @@
 // 用来存放文章信息管理
 
 
+var article = {
+    // 信息的展示
+    category_search: function (options) {
+        $.ajax({
+            url: CATEGORY_SEARCH,
+            success: options.callback
+        });
+    },
+
+    // 分类数据的新增功能
+
+    category_add: function (options) {
+        $.ajax({
+            type: 'post',
+            url: CATEGORY_ADD,
+            data: options.data,
+            success: options.callback
+        });
+    },
+
+
+    // 分类数据的编辑功能
+    // 
+    category_edit: function (options) {
+        $.ajax({
+            type: 'post',
+            url: CATEGORY_EDIT,
+            data: options.data,
+            success: options.callback
+
+        });
+    },
+
+
+    // 分类数据的删除功能
+
+    category_delete: function (options) {
+        $.ajax({
+            type: 'post',
+            url: CATEGORY_DELETE,
+            data: options.data,
+            success: options.callback
+        });
+
+    }
+
+
+};
+
+
